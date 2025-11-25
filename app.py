4eb6cc1bc72cc224771a79ff1d69ebf20d3db734
Alexandre Alcindo <xandoliveira2@users.noreply.github.com>
2025-11-24 23:48:11 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("Meu App de IA")
+user_input = st.text_input("Digite seu prompt")
+if st.button("Gerar Resposta"):
+    # response = funcao_gemini(user_input)
+
+    # st.write(response)
+    pass
